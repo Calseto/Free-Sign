2fc0bd9d67447af4a3477a827b21de92af0627bb
Calseto <72954173+calseto@users.noreply.github.com>
2020-12-20 16:12:13 +0700

    ada yang ketinggalan

---
+++ app/src/main/java/com/freesign/UserProfile.kt
@@ -0,0 +1,37 @@
+package com.freesign
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class UserProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+        Log.d("hello", "hello")
+
+
+        return inflater.inflate(R.layout.user_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        view.findViewById<TextView>(R.id.profile_user_nextbutton).setOnClickListener {
+            
+        }
+    }
+}
